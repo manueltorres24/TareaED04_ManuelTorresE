f74b304dbb3681461a6a658817ac0562a1b94a9e
Manuel <Manuel@Msito>
2020-03-02 18:53:27 +0100

    vfdsv

--- TareaED04_ManuelTorres/src/tareaed04_manueltorres/TareaED04_ManuelTorres.java
+++ TareaED04_ManuelTorres/src/tareaed04_manueltorres/TareaED04_ManuelTorres.java
@@ -73,19 +73,6 @@ public class TareaED04_ManuelTorres {
 			}
 		}
 
-		imprime(contadorCaras, contadorCruces, maximoCarasDoblesConsecutivas, maximoCrucesDoblesConsecutivas);
-	}
-
-	/**
-	 * @param contadorCaras
-	 * @param contadorCruces
-	 * @param maximoCarasDoblesConsecutivas
-	 * @param maximoCrucesDoblesConsecutivas
-	 */
-	public static void imprime(int contadorCaras, int contadorCruces, int maximoCarasDoblesConsecutivas,
-			int maximoCrucesDoblesConsecutivas) {
-		System.out.println("Caras: " + contadorCaras + ", Cruces: " + contadorCruces);
-		System.out.println("Maximo de caras dobles consecutivas: " + maximoCarasDoblesConsecutivas);
-		System.out.println("Maximo de cruces dobles consecutivas: " + maximoCrucesDoblesConsecutivas);
+		extraer.imprime(contadorCaras, contadorCruces, maximoCarasDoblesConsecutivas, maximoCrucesDoblesConsecutivas);
 	}
 }
